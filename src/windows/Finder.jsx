73ffca5d8fc70a4badab9fc3a6215e04189e97a3
aarav-g-168 <guptaaarav987@gmail.com>
2026-08-17 17:05:51 +0530

    Add sidebar with Favorites and Work sections to Finder component

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -1,13 +1,47 @@
 import { Search } from 'lucide-react'
 import WindowControls from '../components/WindowControls'
 import WindowWrapper from '../hoc/WindowWrapper'
+import useLocationStore from '../store/location'
+import { locations } from '../constants/index'
 
 const Finder = () => {
+
+  const { activeLocation, setActiveLocation } = useLocationStore();
+
   return (
     <>
         <div id="window-header">
             <WindowControls windowKey="finder" />
             <Search className="icon" />
+        </div>
+
+        <div className="bg-white flex h-full">
+          <div className="sidebar">
+
+            <div>
+              <h3>Favorites</h3>
+              <ul>
+                {Object.values(locations).map((item) => (
+                  <li key={item.id} onClick={() => setActiveLocation(item)}>
+                    <img src={item.icon} className="w-4" alt={item.name} />
+                    <p className="text-sm font-medium truncate">{item.name}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <h3>Work</h3>
+              <ul>
+                {Object.values(locations).map((item) => (
+                  <li key={item.id} onClick={() => setActiveLocation(item)}>
+                    {item.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+          </div>
         </div>
     </>
   )
