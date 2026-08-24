47338b06d42ca4e736e16d648a3c62f54cce187a
aarav-g-168 <guptaaarav987@gmail.com>
2026-08-24 23:47:39 +0530

    Enhance Finder component: Integrate openItem function for improved item handling and update list rendering

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -4,8 +4,11 @@ import WindowWrapper from '../hoc/WindowWrapper'
 import useLocationStore from '../store/location'
 import { locations } from '../constants/index'
 import clsx from 'clsx'
+import useWindowStore from '../store/window'
 
 const Finder = () => {
+
+  const { openWindow } = useWindowStore();
 
   const { activeLocation, setActiveLocation } = useLocationStore();
 
@@ -23,6 +26,13 @@ const Finder = () => {
     </div>
   )
 
+  const openItem = (item) => {
+    if (item.fileType === 'pdf') return openWindow("resume");
+    if (item.kind === 'folder') return setActiveLocation(item);
+    if (['fig', 'url'].includes(item.fileType) && item.href) return window.open(item.href, '_blank');
+    openWindow(`${item.fileType}${item.kind}`, item);
+  };
+
   return (
     <>
       <div id="window-header">
@@ -33,11 +43,19 @@ const Finder = () => {
       <div className="bg-white flex h-full">
         <div className="sidebar">
           {renderList("Favorites", Object.values(locations))}
-          {renderList("Work", locations.work.children)}
+          {renderList("Projects", locations.work.children)}
         </div>
-      </div>
 
-      
+        <ul className="content">
+          {activeLocation?.children.map((item) => (
+            <li key={item.id} className={item.position} onClick={() => openItem(item)}>
+              <img src={item.icon} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </>
   )
 }
